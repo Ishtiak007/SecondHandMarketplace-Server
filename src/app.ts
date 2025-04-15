5620649a3dee2c6b802e9cf13a6d26a45572f415
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-15 13:12:32 +0600

    extend Express Request interface to include user property

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import router from './app/routes';
 import { globalErrorHandler } from './app/middleware/globalErrorHandler';
+import notFound from './app/middleware/notFound';
 
 const app: Application = express();
 
