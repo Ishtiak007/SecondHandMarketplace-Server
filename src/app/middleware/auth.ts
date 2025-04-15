ca15d0f2bfcf34c403a6e015c5179a4593d43f0f
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-15 21:16:07 +0600

    implement JWT-based auth middleware with role checks, user validation, and soft-delete handling

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -2,58 +2,47 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '../errors/HttpError';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
-import { User } from '../modules/User/user.model';
-import { TUserRole } from '../modules/User/user.interface';
 import { asyncHandler } from '../utils/global/asyncHandler';
+import { TUserRole } from '../modules/User/user.interface';
+import { User } from '../modules/User/user.model';
 
 export const auth = (...requiredRoles: TUserRole[]) => {
   return asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const token = req.headers.authorization;
-
-      // // check if no token
       if (!token) {
         throw new HttpError(
           401,
           'Access token is missing or invalid. Please provide a valid token to access this resource.',
         );
       }
-
-      // // token verify
       const decoded = jwt.verify(
         token,
         config.jwt_access_secret as string,
       ) as JwtPayload;
-      // console.log(decoded)
 
       const { identifier, role } = decoded;
-
-      // check if the user exists using the identifier
       const user = await User.isUserExists(identifier);
       if (!user) {
         throw new HttpError(
           404,
-          'Invalid credentials or session. Please try logging in again',
+          'Your session has expired or your credentials are incorrect. Please log in again.',
         );
       }
-
-      // check if the user is already deleted
       if (user.isDeleted) {
         throw new HttpError(404, 'The user is already deleted');
       }
-
-      // // check if the user is banned
       if (user.status === 'banned') {
         throw new HttpError(
           403,
-          'Your account has been banned. Please contact support for assistance.',
+          'You are no longer able to access this account. Please reach out to support.',
         );
       }
 
       if (requiredRoles && !requiredRoles.includes(role)) {
         throw new HttpError(
           403,
-          'Access denied. Your role does not have the necessary permissions to perform this action.',
+          'Permission denied. Your current role can’t perform this action.',
         );
       }
 
