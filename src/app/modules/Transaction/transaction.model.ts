18450b45f76a3b7315a29d55cb48b019dd81d647
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-18 14:18:59 +0600

    create Transaction schema and model with buyer, seller, item, and payment details

---
+++ src/app/modules/Transaction/transaction.model.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from 'mongoose';
+
+const transactionSchema = new Schema<TTransaction>(
+  {
+    buyerID: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    sellerID: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    itemID: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: 'Listing',
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ['pending', 'completed'],
+        message: '{VALUE} is not a valid status',
+      },
+      default: 'pending',
+    },
+    paymentMethod: {
+      type: String,
+      enum: {
+        values: ['online'],
+        message: '{VALUE} is not a valid payment method',
+      },
+      default: 'online',
+    },
+    transactionId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Transaction = model<TTransaction>(
+  'Transaction',
+  transactionSchema,
+);
